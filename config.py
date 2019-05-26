159673c5ede4584e9ace29aa803a72218d9ab454
Lorddoyo <johnkojo7777@gmail.com>
2019-05-26 19:30:44 +0300

    add SQLALCHEMY_DATABASE_URI, UPLOADED_PHOTOS_DEST

--- config.py
+++ config.py
@@ -1,23 +1,44 @@
 import os
 
+
 class Config:
     '''
     General configuration parent class
     '''
-    BASE_URL=''
-    API_KEY=os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY=os.environ.get('SECRET_KEY')
-    
-    
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorddoyo:adanog@localhost/pitc'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("johnkojo7777@gmail.com")
+    MAIL_PASSWORD = os.environ.get("solarboy98")
+    SUBJECT_PREFIX = 'PITCH'
+    SENDER_EMAIL = ('johnkojo7777@gmail.com')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    # simplemde confirgurations
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 
 class ProdConfig(Config):
     '''
     Production configuration child class
-    
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
     pass
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorddoyo:adanog@localhost/pitc_test'
+
 
 class DevConfig(Config):
     '''
@@ -26,11 +47,11 @@ class DevConfig(Config):
     Args:
         Config : the parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorddoyo:adanog@localhost/pitc'
     DEBUG = True
 
 
-
-config_options={
-    'development':DevConfig,
-    'production':ProdConfig
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
 }
