2f89a882c86b484f742f636022f7161148cf92f8
Lorddoyo <johnkojo7777@gmail.com>
2019-05-27 17:02:42 +0300

    deployment

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     '''
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorddoyo:adanog@localhost/pitc'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorddoyo:adanog@localhost/pitc'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
     # email configurations
@@ -33,7 +33,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestConfig(Config):
